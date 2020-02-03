6b2a8ed4ef1478a6c8f4f9293f286e8cba1882fa
Samuel S Ravella <samuelravella11@gmail.com>
2020-02-03 17:59:14 +0530

    second
    add todos

--- goal-app/App.js
+++ goal-app/App.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [ enteredGoal, setEnteredGoal ] = useState('');
+	const [ courseGoals, setCourseGoals ] = useState([]);
+	const handleText = (enteredText) => {
+		setEnteredGoal(enteredText);
+	};
+	const addGoal = () => {
+		setCourseGoals((currentGoals) => [ ...currentGoals, enteredGoal ]);
+	};
+
+	return (
+		<View style={styles.view}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder="Course goal"
+					style={styles.input}
+					onChangeText={handleText}
+					value={enteredGoal}
+				/>
+				<Button title="ADD" color="gray" onPress={addGoal} />
+			</View>
+			<View>
+				{courseGoals.map((goal, index) => {
+					return (
+						<Text key={index} style={styles.input}>
+							{goal}
+						</Text>
+					);
+				})}
+			</View>
+			<View />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	view: {
+		padding: 75
+	},
+	inputContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+	},
+	input: {
+		width: '75%',
+		padding: 10,
+		borderColor: 'black',
+		borderWidth: 2
+	}
 });
